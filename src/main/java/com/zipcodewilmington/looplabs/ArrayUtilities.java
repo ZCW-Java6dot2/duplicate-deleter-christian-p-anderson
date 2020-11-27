0603194ca8f26d12e56ffb000b3b228faaefb234
christian <christian@zipcodes-MBP-51.lan>
2020-11-26 23:12:09 -0500

    refactored code in ArrayUtilities class

--- src/main/java/com/zipcodewilmington/looplabs/ArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/looplabs/ArrayUtilities.java
@@ -5,50 +5,10 @@ import java.util.Arrays;
 
 public class ArrayUtilities<T> {
 
-    private T[] content;
+    private T[] arrContent;
 
-    public ArrayUtilities(T[] content) {
-        this.content = content;
-    }
-
-    /**
-     * helper method
-     * @param arr is the array being evaluated
-     * @param value is the value that needs to be removed
-     * @return arr2 which doesn't contain the value
-     */
-    public T[] removeDuplicatesHelper(T[] arr, T value) {
-        int countOfValue = countValues(arr, value);
-
-        int index = 0;
-
-        // create a new array based on the specified component type and length
-        T[] arr2 = (T[]) Array.newInstance(content.getClass().getComponentType(), arr.length - countOfValue);
-
-        for (T element : arr) {
-            if(!element.equals(value)) {
-                arr2[index++] = element;
-            }
-        }
-
-        return arr2;
-    }
-
-    /**
-     * this is a helper method that helps determine the length of arr2 in the removeDuplicates() method above and also used in the methods below
-     * @param arr is the array to be evaluated
-     * @param value is the value needs to be counted
-     * @return how many times the value of type T occurs in the array
-     */
-    public int countValues(T[] arr, T value) {
-        int countOfValue = 0;
-
-        for (T element : arr) {
-            if (element.equals(value)) {
-                countOfValue++;
-            }
-        }
-        return countOfValue;
+    public ArrayUtilities(T[] arrContent) {
+        this.arrContent = arrContent;
     }
 
     /**
@@ -81,6 +41,46 @@ public class ArrayUtilities<T> {
             }
         }
         return arr2;
+    }
+
+    /**
+     * helper method
+     * @param arr is the array being evaluated
+     * @param value is the value that needs to be removed
+     * @return arr2 which doesn't contain the value
+     */
+    public T[] removeDuplicatesHelper(T[] arr, T value) {
+        int countOfValue = countValues(arr, value);
+
+        int index = 0;
+
+        // create a new array based on the specified component type and length
+        T[] arr2 = (T[]) Array.newInstance(arrContent.getClass().getComponentType(), arr.length - countOfValue);
+
+        for (T element : arr) {
+            if(!element.equals(value)) {
+                arr2[index++] = element;
+            }
+        }
+
+        return arr2;
+    }
+
+    /**
+     * this is a helper method that helps determine the length of arr2 in the removeDuplicates() method above and also used in the methods above
+     * @param arr is the array to be evaluated
+     * @param value is the value needs to be counted
+     * @return how many times the value of type T occurs in the array
+     */
+    public int countValues(T[] arr, T value) {
+        int countOfValue = 0;
+
+        for (T element : arr) {
+            if (element.equals(value)) {
+                countOfValue++;
+            }
+        }
+        return countOfValue;
     }
 
 }
